f83ef37bc28d0fd6c248cae6c9b64a9ac7f7cd35
Markjonel02 <markjonel@neksjob.com>
2024-02-19 23:36:34 +0800

    modified:   index.css 	modified:   src/assets/pages/About.tsx

--- src/assets/pages/About.tsx
+++ src/assets/pages/About.tsx
@@ -1,7 +1,7 @@
 import about from "../img/pexels-tima-miroshnichenko-7991269.jpg";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const imgProperty = {
   radius: 20,
@@ -9,13 +9,47 @@ const imgProperty = {
   alt: "aboutimage",
 };
 
+type FAQItem = {
+  question: string;
+  answer: string;
+};
 const About = () => {
+  const [rotatedIndexes, setRotatedIndexes] = useState<number[]>([]);
+
+  const faqItems: FAQItem[] = [
+    {
+      question: "Can I download movies and TV shows for offline viewing?",
+      answer:
+        "Yes, MovieFlix allows users to download select movies and TV shows for offline viewing. This feature is available on the MovieFlix app for mobile devices. Simply find the title you want to download, and look for the download icon next to it.",
+    },
+    {
+      question: "Are there different subscription plans available?",
+      answer:
+        "MovieFlix offers different subscription tiers to cater to varying user needs. These may include options for streaming in HD, simultaneous streaming on multiple devices, and ad-free viewing experiences. Pricing and features of each plan may vary.",
+    },
+    {
+      question: "Can I cancel my MovieFlix subscription at any time?",
+      answer:
+        "Yes, you can cancel your MovieFlix subscription at any time. There are no long-term contracts, and you can easily manage your subscription settings through your account dashboard. Keep in mind that cancellation may result in the loss of access to the MovieFlix content library.",
+    },
+  ];
+  // Function to handle click event on FAQ item
+  const handleRotate = (index: number) => {
+    // If the index is already rotated, remove it from the state
+    if (rotatedIndexes.includes(index)) {
+      setRotatedIndexes(rotatedIndexes.filter((i) => i !== index));
+    } else {
+      // If the index is not rotated, add it to the state
+      setRotatedIndexes([...rotatedIndexes, index]);
+    }
+  };
   useEffect(() => {
     Aos.init();
     return () => {
       Aos.refresh();
     };
   }, []);
+
   return (
     <>
       <div className="about-container about">
@@ -175,196 +209,86 @@ const About = () => {
           </div>
         </div>
 
-        {/* FAQ SEction */}
-        <div className="FAQ container d-flex flex-column mb-5 py-5">
-          <div className="faq-title text-light ">
-            <h1 className="fs-2 mb-4" style={{ fontWeight: "800" }}>
-              MovieFlix FAQ
-            </h1>
-          </div>
-          <div className="wrapper d-flex justify-content-between align-items-center flex-column mb-4">
-            <div className="faq-clicker w-100 d-flex justify-content-between   p-2  ">
-              <h4 className="text-center d-flex justify-content-center  align-items-center ">
-                Q: Can I download movies and TV shows for offline viewing?
-              </h4>
-              <button className="circular-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="tranparent"
-                  className=" plus w-6 h-6 "
+        {/* FAQ SECTION */}
+        <div className="FAQ container d-flex flex-column mb-5 py">
+          <h1 className=" text-light fw-bolder mb-4">MovieFlix FAQ</h1>
+          {/* Render FAQ items */}
+          {faqItems.map((item, index) => (
+            <div
+              key={index}
+              className="wrapper d-flex justify-content-between align-items-center flex-column mb-4"
+            >
+              {/* FAQ question */}
+              <div className="faq-clicker w-100 d-flex justify-content-between p-2">
+                <h4 className="text-center d-flex justify-content-center align-items-center ">
+                  {item.question}
+                </h4>
+                {/* Button to rotate FAQ item */}
+                <button
+                  onClick={() => handleRotate(index)}
+                  className={`circular-button ${
+                    rotatedIndexes.includes(index) ? "rotated" : ""
+                  }`}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="plus w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+              {/* FAQ answer - Conditionally render based on rotatedIndexes state */}
+              {rotatedIndexes.includes(index) && (
+                <div className="plus-clicked active" data-aos="fade-down">
+                  <ul>
+                    <li>
+                      <p className="para">{item.answer}</p>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
-            <div className="plus-clicked ">
-              <ul>
-                <li>
-                  <p className="para">
-                    A: Yes, MovieFlix allows users to download select movies and
-                    TV shows for offline viewing. This feature is available on
-                    the MovieFlix app for mobile devices. Simply find the title
-                    you want to download, and look for the download icon next to
-                    it.
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="wrapper d-flex justify-content-between align-items-center flex-column mb-4">
-            <div className="faq-clicker w-100 d-flex justify-content-between   p-2  ">
-              <h4 className="text-center d-flex justify-content-center  align-items-center ">
-                Q: Are there different subscription plans available?
-              </h4>
-              <button className="circular-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className=" plus w-6 h-6 "
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="plus-clicked ">
-              <ul>
-                <li>
-                  <p className="para">
-                    A: MovieFlix offers different subscription tiers to cater to
-                    varying user needs. These may include options for streaming
-                    in HD, simultaneous streaming on multiple devices, and
-                    ad-free viewing experiences. Pricing and features of each
-                    plan may vary.
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="wrapper d-flex justify-content-between align-items-center flex-column mb-4">
-            <div className="faq-clicker w-100 d-flex justify-content-between   p-2  ">
-              <h4 className="text-center d-flex justify-content-center  align-items-center ">
-                Q: Can I cancel my MovieFlix subscription at any time?
-              </h4>
-              <button className="circular-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className=" plus w-6 h-6 "
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="plus-clicked ">
-              <ul>
-                <li>
-                  <p className="para">
-                    A: Yes, you can cancel your MovieFlix subscription at any
-                    time. There are no long-term contracts, and you can easily
-                    manage your subscription settings through your account
-                    dashboard. Keep in mind that cancellation may result in the
-                    loss of access to the MovieFlix content library.
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="wrapper d-flex justify-content-between align-items-center flex-column mb-4">
-            <div className="faq-clicker w-100 d-flex justify-content-between   p-2  ">
-              <h4 className="text-center d-flex justify-content-center  align-items-center ">
-                Q: Are there different subscription plans available?
-              </h4>
-              <button className="circular-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className=" plus w-6 h-6 "
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="plus-clicked ">
-              <ul>
-                <li>
-                  <p className="para">
-                    A: MovieFlix offers different subscription tiers to cater to
-                    varying user needs. These may include options for streaming
-                    in HD, simultaneous streaming on multiple devices, and
-                    ad-free viewing experiences. Pricing and features of each
-                    plan may vary.
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
+          ))}
         </div>
+
         {/* Contact Us */}
 
-        <div className="container">
-          <div className="row">
-            {/* Contact Section */}
-            <div className="col-md-6">
-              <div className="Contact wrapper d-flex flex-column">
-                <div className="Contact-content">
-                  <h1 className="ctitles text-light">Contact Us</h1>
-                  <div className="Phone">
-                    <p>Phone</p>
-                    <a href="tel:+1-555-555-5555">222</a>
-                  </div>
-                  <div className="Email">
-                    <p>Email</p>
-                    <a href="mailto:info@movieflix.com">info@movieflix.com</a>
-                  </div>
-                </div>
+        <div className="Contact container">
+          <div className="wrapper d-flex flex-column ">
+            <div className="Contact-content">
+              <h1 className="ctitles text-light">Contact Us</h1>
+              <div className="Phone">
+                <p>Phone</p>
+                <a href="tel:+1-555-555-5555">222</a>
+              </div>
+              <div className="Email">
+                <p>Email</p>
+                <a href="mailto:info@movieflix.com">info@movieflix.com</a>
               </div>
             </div>
-            {/* Form Section */}
-            <div className="col-md-6">
-              <div className="formemail">
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label htmlFor="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="floatingPassword"
-                    placeholder="Password"
-                  />
-                  <label htmlFor="floatingPassword">Password</label>
-                </div>
-              </div>
-            </div>
+          </div>
+          <div className="formemail">
+            <form action="" className="">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+            </form>
           </div>
         </div>
       </div>
