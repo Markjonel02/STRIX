c58f74dada51ee76814cffc37e5348818bfa9532
Markjonel02 <markjonel@neksjob.com>
2024-03-10 23:00:34 +0800

    modified:   src/assets/pages/About.tsx

--- src/assets/pages/About.tsx
+++ src/assets/pages/About.tsx
@@ -429,32 +429,32 @@ const About = () => {
           ))}
         </div>
 
-        {email && (
-          <div className="checkmark-container position-relative text-center">
-            <svg
-              className="checkmark"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 52 52"
-            >
-              <circle
-                className="checkmark__circle"
-                cx="26"
-                cy="26"
-                r="25"
-                fill="none"
-              />
-              <path
-                className="checkmark__check"
-                fill="none"
-                d="M14.1 27.2l7.1 7.2 16.7-16.8"
-              />
-            </svg>
-            <p className="message text-light">Message Sent!</p>
-          </div>
-        )}
-
         {/* Contact Us */}
         <div className="Contact container">
+          {" "}
+          {email && (
+            <div className="checkmark-container position-relative text-center">
+              <svg
+                className="checkmark"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 52 52"
+              >
+                <circle
+                  className="checkmark__circle"
+                  cx="26"
+                  cy="26"
+                  r="25"
+                  fill="none"
+                />
+                <path
+                  className="checkmark__check"
+                  fill="none"
+                  d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                />
+              </svg>
+              <p className="message text-light">Message Sent!</p>
+            </div>
+          )}
           <div className="row">
             <div className="col-md-6" data-aos="fade-right">
               <div className="wrapper">
